4bfe2fc143684d60c686042b4105b9180f814791
Sukanta Samanta <sukantasamanta2003@gmail.com>
2023-09-24 16:13:31 +0530

    Update MyFirstGithubClass.java
    new

--- JavaTest/src/com/java/generic/MyFirstGithubClass.java
+++ JavaTest/src/com/java/generic/MyFirstGithubClass.java
@@ -4,7 +4,8 @@ public class MyFirstGithubClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
-		System.out.println("I have learn Github!!");
+		System.out.println("I have learned Github!!");
+		System.out.println("I have learned Github!!");
 	}
 
 }
